c13351a9a7b0cc5751faa4b8260a5357f822ae4e
khakhiD <kakkiid@gmail.com>
2024-04-17 22:11:11 +0900

    feat: 기존 프로젝트 + 타입스크립트 설치

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+const API_END_POINT = import.meta.env.VITE_API_END_POINT
+const API_USERNAME = import.meta.env.VITE_API_USERNAME
+
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        "x-username": API_USERNAME,
+        "Content-Type": "application/json",
+      },
+    })
+    if (res.ok) {
+      const json = await res.json()
+      return json
+    } else {
+      throw new Error(`API Fetch Error Code: ${res.status}`)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
