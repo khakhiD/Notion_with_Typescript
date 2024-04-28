79ea3c22dc2c21bd066ee1025175bb80f1db1dde
khakhiD <kakkiid@gmail.com>
2024-04-28 17:56:40 +0900

    refactor: DocumentDetail, DocumentDetails 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
+export interface DocumentDetail {
+  title: string
+  content: string
+}
+
+export interface DocumentDetails {
+  id: string
+  title: string
+  content: string
+}
+
 export interface Document {
   id: number
   title: string
